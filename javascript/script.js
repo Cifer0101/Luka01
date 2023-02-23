7ad9b8b1a6d5d937d4e1d24e80681aecb8bd0eed
Cifer0101 <l.kakabadze2020@gmail.com>
2023-02-23 16:35:34 +0400

    JS

---
+++ javascript/script.js
@@ -0,0 +1,22 @@
+// let salary = prompt("salary")
+// if(salary<1000){
+//     alert("below average salary")
+// }
+// else if(salary>=1000 && salary <2000){
+//     alert("average salary")
+// }
+// else if(salary>=2000 && salary <4000){
+//     alert("above average salary")
+// }
+
+
+let number = prompt[1,4,18,24,15,2,3,19,55]
+if(number=1,3,15,19,55){
+    alert("the number is odd")
+}
+else if(number = 55){
+    alert("the biggest number")
+}
+else if(number = 1){
+    alert("the smallest number")
+}
